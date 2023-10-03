937625cafe3f63cf286ed4719dae1d3d10cfbc82
Lysenko <lysenkoden@gmail.com>
2023-10-03 12:53:42 +0200

    add logic and img

--- index.js
+++ index.js
@@ -31,12 +31,12 @@
 // console.log(user6); // object
 
 // homework:
-// 1. create primitive lottery.
+//* 1. create primitive lottery.
 //* 2. create squares with digits.
 //* 3. user have only 3 tries.
 //* 4. if user guess - alert 'you win' and clean up the page .
-// 5. if user not guess  - alert 'try one more time (2 times)'.
-// 6. if user not guess 3 times - alert 'you lose' and reset all squares.
+//* 5. if user not guess  - alert 'try one more time (2 times)'.
+//* 6. if user not guess 3 times - alert 'you lose' and reset all squares.
 //* 7. do not allow user click the same button one more time.
 
 function CreateSquares(number, isWin) {
@@ -45,56 +45,115 @@ function CreateSquares(number, isWin) {
   numTries = 0;
 }
 
-const square1 = new CreateSquares(1, true);
-const square2 = new CreateSquares(2, false);
-const square3 = new CreateSquares(3, false);
-const square4 = new CreateSquares(4, false);
-const square5 = new CreateSquares(5, false);
-const square6 = new CreateSquares(6, false);
-const square7 = new CreateSquares(7, false);
-const square8 = new CreateSquares(8, false);
-const square9 = new CreateSquares(9, false);
+let win1, win2, win3, win4, win5, win6, win7, win8, win9;
+const numRandom = Math.floor(Math.random() * 10);
+
+switch (numRandom) {
+  case 1:
+    win1 = 1;
+    break;
+  case 2:
+    win2 = 1;
+    break;
+  case 3:
+    win3 = 1;
+    break;
+  case 4:
+    win4 = 1;
+    break;
+  case 5:
+    win5 = 1;
+    break;
+  case 6:
+    win6 = 1;
+    break;
+  case 7:
+    win7 = 1;
+    break;
+  case 8:
+    win8 = 1;
+    break;
+  case 9:
+    win9 = 1;
+    break;
+  default:
+    break;
+}
+
+const square1 = new CreateSquares(1, win1);
+const square2 = new CreateSquares(2, win2);
+const square3 = new CreateSquares(3, win3);
+const square4 = new CreateSquares(4, win4);
+const square5 = new CreateSquares(5, win5);
+const square6 = new CreateSquares(6, win6);
+const square7 = new CreateSquares(7, win7);
+const square8 = new CreateSquares(8, win8);
+const square9 = new CreateSquares(9, win9);
 
 function renderSquares(objSquare) {
+  // const h2Message = document.createElement(h2);
   const container = document.querySelector(".squares-container");
   const square = document.createElement("div");
+  // const pAuthor = document.createElement("p");
   square.classList.add("square");
   square.innerText = objSquare.number;
 
   const clickHandler = () => {
     numTries++;
     square.classList.add("disabled");
-    if (objSquare.isWin) {
+    if (objSquare.isWin && numTries <= 3) {
       square.classList.add("square-right");
-      alert(
-        `Hey, congratulations you have WON with ${numTries} attempt(s) from 3!!!`
-      );
+      // alert(
+      //   `Hey, congratulations you have WON with ${numTries} attempt(s) from 3!!!`
+      // );
       setTimeout(() => {
         document.body.innerHTML = "";
         const greetings = document.createElement("h1");
         const buttonRestart = document.createElement("button");
         document.body.appendChild(greetings);
         greetings.innerHTML = "congratulations, you`re lucky ass!";
-        // const img = document.createElement("img");
-        // img.setAttribute(
-        //   "href",
-        //   "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fyou-win.html&psig=AOvVaw2e4r1_mkm5DoF1w4EVMm9q&ust=1696365303989000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJjtgbSb2IEDFQAAAAAdAAAAABAD"
-        // );
+
+        const img = document.createElement("img");
+        img.setAttribute("src", "./img/win.png");
+        img.setAttribute("alt", "you win!");
+        document.body.appendChild(img);
+
         buttonRestart.innerHTML = "Restart";
         buttonRestart.setAttribute("onClick", "window.location.reload()");
         document.body.appendChild(buttonRestart);
-      }, 1000);
-    } else {
+      }, 500);
+    } else if (numTries < 3) {
       square.classList.add("square-wrong");
-      alert(
-        `Hey, you are NOT win yet with ${numTries} attempt(s) from 3 - try one more time...`
-      );
+      // alert(
+      //   `Hey, you are NOT win yet with ${numTries} attempt(s) from 3 - try one more time...`
+      // );
+    } else if ((numTries = 3)) {
+      square.classList.add("square-wrong");
+      // alert(`Hey, you are NOT win with ${numTries} attempt(s) from 3 - loser!`);
+      setTimeout(() => {
+        document.body.innerHTML = "";
+        const greetings = document.createElement("h1");
+        const buttonRestart = document.createElement("button");
+        document.body.appendChild(greetings);
+        greetings.innerHTML = "May be you get luck in other time...";
+
+        const img = document.createElement("img");
+        img.setAttribute("src", "./img/lose.png");
+        img.setAttribute("alt", "you lose!");
+        document.body.appendChild(img);
+
+        buttonRestart.innerHTML = "Restart";
+        buttonRestart.setAttribute("onClick", "window.location.reload()");
+        document.body.appendChild(buttonRestart);
+      }, 500);
     }
   };
 
   square.onclick = () => clickHandler();
 
   container.appendChild(square);
+  // body.appendChild(pAuthor);
+
   return;
 }
 
